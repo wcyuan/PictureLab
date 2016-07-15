bab26dfb6faee6247a4fc9ca2993f513c69669ed
Conan Yuan <conanyuan@gmail.com>
2016-07-15 03:38:36 -0400

    A4

--- pixLab/classes/IntArrayWorker.java
+++ pixLab/classes/IntArrayWorker.java
@@ -99,4 +99,48 @@ public class IntArrayWorker
     }
   }
  
+  public int getCount(int num)
+  {
+	  int count = 0;
+	  for (int row = 0; row < matrix.length; row++)
+	  {
+		  for (int col = 0; col < matrix[0].length; col++)
+		  {
+	    	  if (matrix[row][col] == num) {
+	    		  count++;
+	    	  }
+	      }
+	  }
+	  return count;
+  }
+
+  public int getLargest()
+  {
+	  int largest = matrix[0][0];
+	  for (int row = 0; row < matrix.length; row++)
+	  {
+		  for (int col = 0; col < matrix[0].length; col++)
+		  {
+	    	  if (matrix[row][col] > largest) {
+	    		  largest = matrix[row][col];
+	    	  }
+	      }
+	  }
+	  return largest;
+  }
+  
+  public int getColTotal(int col)
+  {
+	  int total = 0;
+	  if (col < 0) {
+		  return total;
+	  }
+	  for (int row = 0; row < matrix.length; row++)
+	  {
+		  if (col < matrix[row].length) {
+			  total += matrix[row][col];
+		  }
+	  }
+	  return total;
+  }
 }
